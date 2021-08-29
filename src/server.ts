2d6b58ba2e05e7759f1ce0fa03da20f2db2848dd
Jeferson Rodrigues <jefersonr.santos@outlook.com>
2021-08-29 14:15:24 -0300

    fineshed config project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express from "express";
 
-const server = express()
+const server = express();
+server.use(express.json());
 
-server.get("/", (request, response) => {
-    response.json({ message: "Funcionou corretamente!" })
-})
+server.get("/courses", (request, response) => {
+    response.json({ message: "Funcionou corretamente!" });
+});
 
-server.listen(3333, () => console.log("Server is runnig!"))
+server.post("/courses", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
+});
+
+server.listen(3333, () => console.log("Server is running!"));
