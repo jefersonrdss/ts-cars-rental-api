be4da265e4b316604658b99937c8605701642c75
Jeferson Rodrigues <jefersonr.santos@outlook.com>
2021-09-02 21:14:57 -0300

    add package multer on project

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
+const upload = multer({ dest: "./tmp" });
 const categoriesRoutes = Router();
 
 // Route for create categories
@@ -13,5 +15,10 @@ categoriesRoutes.post("/categories", (request, response) => {
 categoriesRoutes.get("/categories", (request, response) => {
     return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post("/categories/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    return response.status(200).json(file);
+})
 
 export { categoriesRoutes };
